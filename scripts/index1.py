ac0b68846a43194f3e9ce61ce9afcea2cd131a08
omkarkeshav <omkarkeshav1073@gmail.com>
2023-12-16 19:05:59 +0530

    Added python code to index1 script

--- scripts/index1.py
+++ scripts/index1.py
@@ -1,1 +1,16 @@
 print("Hello World")
+
+def count_characters(input_string):
+    char_count = {}
+    
+    for char in input_string:
+        char_count[char] = char_count.get(char,0) + 1
+    
+    return char_count
+
+input_string = "you are so intelligent man"
+
+result = count_characters(input_string) 
+
+for char, count in result.items():
+    print(f"Character '{char}' occurs {count} times.")
