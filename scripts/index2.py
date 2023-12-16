5482c2a4f45d287f8c9efbccdd56cf289c997a7e
omkarkeshav <omkarkeshav1073@gmail.com>
2023-12-16 18:31:51 +0530

    Added new python code into the index2 script

--- scripts/index2.py
+++ scripts/index2.py
@@ -1,6 +1,22 @@
+'''
 keys = ['name','age','city']
 values = ['John', 25, 'New York']
 
 my_dict = dict(zip(keys,values))
 
 print(my_dict)
+'''
+
+def fibonacci(n, memo={}):
+    if n in memo:
+        return memo[n]
+    elif n <= 1:
+        return 1
+    else:
+        result = fibonacci(n-1, memo) + fibonacci(n-2, memo)
+        memo[n] = result
+        return result
+
+n = 10
+result = fibonacci(n)
+print(f"The {n}-th Fibonacci number is: {result}")
